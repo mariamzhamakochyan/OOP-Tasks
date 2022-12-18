e8ed9c5226764db0b80d51117edd3db7cc09888d
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-12-18 19:55:33 +0400

    Create helicopter.py

---
+++ helicopter.py
@@ -0,0 +1,33 @@
+class Helicopter:
+    speed = 0
+    started = False
+    def __init__(self,manufacturer,year,name,price):
+        self.year = year
+        self.name = name
+        self.price = price
+        self.manufacturer = manufacturer
+    
+    def start(self):
+        self.started = True
+        print("Helicopter started, let's fly!")
+
+    def increase_speed(self, delta):
+        if self.started:
+            self.speed = self.speed + delta
+            print('swish/swash/swish/swash.')
+        else:
+            print("You need to start the Helicopter first")
+
+    def stop(self):
+        self.speed = 0
+        print('Halting')
+    
+    def showDescription(self):
+        print(f"Manufacturer: {self.manufacturer} \nHelicopter name: {self.name} \nIntroduction:  {self.year} \nPrice: {self.price}")
+        
+        
+h = Helicopter("Bell Helicopter","2018", "Bell 429", "$6,975,000")
+h.showDescription()
+h.increase_speed(10)
+h.start()
+h.increase_speed(40)

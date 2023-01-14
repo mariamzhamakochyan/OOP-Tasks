b5eeac2d6b92ce73386f94f44ea3aafe703ad065
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-01-14 22:36:16 +0400

    Create Account.py

---
+++ Irv_Kalb/Bank Account Simulations/Account.py
@@ -0,0 +1,41 @@
+class Account:
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = balance
+        self.password = password
+    def deposite(self, amountoDeposit, password):
+        if password != self.password:
+            print("Incorrect password")
+            return None
+        if amountoDeposit < 0:
+            print('Negative amount')
+            return None
+        else:
+            self.balance += amountoDeposit
+            return self.balance
+    def withdraw(self, amunttoWithdrow, password):
+        if password != self.password:
+            print("Incorrect password")
+            return None
+        if amunttoWithdrow > self.deposite:
+            print('It is more than what you have')
+            return None
+        if amunttoWithdrow > self.deposit:
+            self.balance -= amunttoWithdrow
+            return self.balance
+    def GetBalance(self, password):
+        if password != self.password:
+            print("Incorrect password")
+            return None
+        return self.balance
+    def show(self):
+        print('       Name:', self.name)
+        print('       Balance:', self.balance)
+        print('       Password:', self.password)
+        print()
+A = Account("Maria", 100000, "Brain")
+A.deposite(150000, "Brain")
+A.GetBalance("Brain")
+A.show()
+        
+            

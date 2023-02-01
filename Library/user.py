0c8b112a0a0bc9d903525401646fac3a6ac7c461
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-02 01:59:20 +0400

    Update user.py

--- Library/user.py
+++ Library/user.py
@@ -9,16 +9,9 @@ class User:
         self.user[name] = defaultdict(list)
         
     def add_book(self, name, author, title):
-        n = Library()
-        if author in n.books:
-            
-            aut = self.user[name]
-            title_list = aut[author]
-            title_list.append(title)
-            # else:
-            #     print("ayd vernagrov girq cavoq ays pahin arka chka")
-        else:
-            print("tvyal nhexinakic grqer ays pahin arka chka")
+        aut = self.user[name]
+        title_list = aut[author]
+        title_list.append(title)
             
     def show(self):
         return self.user
@@ -27,7 +20,7 @@ u = User()
 u.add_user("Chūya Nakahara")
 u.add_book('Chūya Nakahara', 'Dazai Osamu', "No longer human")
 u.add_book('Chūya Nakahara', 'Dazai Osamu', "Self-Portraits")
-# u.add_user("Akutagawa")
-# u.add_book('Akutagawa', 'Charles Dickens', "Great Expectations")
-# u.add_book('Akutagawa', 'Charles Dickens', "Hard Times")
-# print(u.show())
+u.add_user("Akutagawa")
+u.add_book('Akutagawa', 'Charles Dickens', "Great Expectations")
+u.add_book('Akutagawa', 'Charles Dickens', "Hard Times")
+print(u.show())

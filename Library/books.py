3906a7e67a0141ceab3df3369ea59c40dcbb83ba
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-01 23:32:28 +0400

    Create books.py

---
+++ Library/books.py
@@ -0,0 +1,16 @@
+class Library:
+    def __init__(self):
+        self.books = {}
+    def add_author(self, author):
+        self.books[author] = []
+    def bookname(self, author, title):
+        self.books[author].append(title)
+    def show(self):
+        return self.books
+
+l = Library()
+l.add_author("Dazai Osamu")
+l.bookname("Dazai Osamu", "No longer human")
+l.bookname("Dazai Osamu", "The setting sun")
+l.bookname("Dazai Osamu", "Self-Portraits")
+print(l.show())
